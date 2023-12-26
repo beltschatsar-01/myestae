fdb14a6ed1175c46d487ca49d3122c4846dc8388
beltschatsar-01 <jecksonbadibolowa@gmail.com>
2023-12-26 18:18:28 +0300

    create listing

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,6 +5,10 @@ import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+  const existingUsername = await User.findOne({ username });
+  if (existingUsername) return next(errorHandler(409, 'You entered an existing "username"!'));
+  const existingEmail = await User.findOne({ email });
+  if (existingEmail) return next(errorHandler(409, 'You entered an existing "email"!'));
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
@@ -19,7 +23,7 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, 'User not found!'));
+    if (!validUser) return next(errorHandler(404, 'Wrong credentials!'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
